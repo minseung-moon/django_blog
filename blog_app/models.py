5b8d59f4e081313d0250127ebda3d91481ecbec9
MinSeung Moon <ansalstmd@gmail.com>
2021-04-06 00:12:39 +0900

    Post 모델에 file_upload 필드 추가하기

--- blog_app/models.py
+++ blog_app/models.py
@@ -9,6 +9,7 @@ class Post(models.Model):
     # upload_to : 이미지를 저장할 폴더의 경로 규칙을 지정, blank=True를 하면 필수 항목이 아니게 된다
     # python -m pip install Pillow
     head_image = models.ImageField(upload_to = 'blog_app/images/%Y/%m/%d/', blank=True)
+    file_upload = models.FileField(upload_to='blog_app/files/%Y/%m/%d/', blank=True)
     # 포스트 생성시 자동으로 저장되는 모델, auto_now_add=True 처음 레코드가 생성될 때 자동으로 저장
     created_at = models.DateTimeField(auto_now_add=True)
     # 수정 시 자동으로 저장되는 모델, auto_now=True 다시 저장할때 자동으로 저장
