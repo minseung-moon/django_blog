8520fda8fac3268294c19af207803f5e787345a6
MinSeung Moon <ansalstmd@gmail.com>
2021-04-08 23:18:31 +0900

    댓글을 포스트 상세 페이지에 나타내기

--- blog_app/admin.py
+++ blog_app/admin.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
-from .models import Post, Category, Tag
+from .models import Post, Category, Tag, Comment
+from markdownx.admin import MarkdownxModelAdmin
 
 # Register your models here.
-admin.site.register(Post)
+admin.site.register(Post, MarkdownxModelAdmin)
+admin.site.register(Comment)
 
 class CategoryAdmin(admin.ModelAdmin):
     # Category 모댈의 name필드에 값이 입력됐을 때 자동으로 slug가 만들어진다
@@ -13,5 +15,3 @@ class TagAdmin(admin.ModelAdmin):
 
 admin.site.register(Category, CategoryAdmin)
 admin.site.register(Tag, TagAdmin)
-
-
